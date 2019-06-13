462a2b1691ca5ffc92ca5a224753790b790f2d84
Laid41798175 <51368746+Laid41798175@users.noreply.github.com>
2019-06-13 16:48:49 +0900

    New One

---
+++ Assets/Scripts/Stage 1/Puzzle Elements/BilliardTable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BilliardTable : MonoBehaviour
+{
+    public GameObject[] balls = new GameObject[7];
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "PuzzleBall") {
+            foreach (GameObject ball in balls) {
+                ball.GetComponent<Rigidbody2D>().gravityScale = 1;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Stage 1/Puzzle Elements/BulbLever.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulbLever : MonoBehaviour
+{
+    public GameObject bulb;
+
+    private bool isReady;
+    public float delay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isReady = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (isReady) {
+            if (collider.tag == "PlayerMelee") {
+                StartCoroutine(Wait());
+                GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
+                bulb.GetComponent<Bulb>().IsReady();
+            }
+        }
+        
+    }
+
+    IEnumerator Wait() {
+        isReady = false;
+        yield return new WaitForSeconds(delay);
+        isReady = true;
+    }
+}
