f4fecd6d07fa41f70f6a384e0ab32f285b6092f1
Laid41798175 <51368746+Laid41798175@users.noreply.github.com>
2019-06-15 16:26:57 +0900

    arrow

---
+++ Assets/Scripts/Stage 1/Puzzle Elements/Arrow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public int index;
+
+    public Sprite[] onSprites = new Sprite[4];
+    private Sprite[] offSprites = new Sprite[4];
+
+    public GameObject[] arrows = new GameObject[2];
+    private GameObject billiardTable;
+
+    private bool isReady;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offSprites[index] = arrows[0].GetComponent<SpriteRenderer>().sprite;
+        billiardTable = GameObject.Find("BilliardTable");
+
+        isReady = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (isReady) {
+            if (collision.tag == "PuzzleBall") {
+                isReady = false;
+                foreach(GameObject arrow in arrows) {
+                    arrow.GetComponent<SpriteRenderer>().sprite = onSprites[index];
+                }
+            }
+        }        
+    }
+
+
+}
