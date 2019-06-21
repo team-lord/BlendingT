b8a1cae999a1498ca5914cab6f21d7f1aabbe04c
jesange <51369461+jesange@users.noreply.github.com>
2019-06-21 14:06:11 +0900

    TrashmobAnimation+

--- Assets/Scripts/Stage 2/Boss2/MakeBeeB2.cs
+++ Assets/Scripts/Stage 2/Boss2/MakeBeeB2.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class MakeBeeB2 : MonoBehaviour
 {
-    public GameObject beeA;
-    public GameObject beeB;
+    public GameObject trashmobNormal;
+    public GameObject trashmobHoney;
 
     public float probability; // 0 ~ 1
 
@@ -29,9 +29,9 @@ public class MakeBeeB2 : MonoBehaviour
     public void MakeBee() {
         if (isReady) {
             if (Random.Range(0f, 1f) < probability) {
-                Instantiate(beeA, transform.position + Vector3.left, transform.rotation); // TODO - 맵 밖에서 생성되어야 함
+                Instantiate(trashmobNormal, transform.position + Vector3.left, transform.rotation); // TODO - 맵 밖에서 생성되어야 함
                 if (isForged) {
-                    Instantiate(beeB, transform.position + Vector3.right, transform.rotation); // TODO - 맵 밖에서 생성되어야 함
+                    Instantiate(trashmobHoney, transform.position + Vector3.right, transform.rotation); // TODO - 맵 밖에서 생성되어야 함
                 }
             }
         }        
