48a94c828be5a6eabf02478f394be1395d2998f6
Laid41798175 <51368746+Laid41798175@users.noreply.github.com>
2019-06-21 15:21:42 +0900

    d

--- Assets/Scripts/Stage 2/Boss2/SweepB2.cs
+++ Assets/Scripts/Stage 2/Boss2/SweepB2.cs
@@ -8,40 +8,24 @@ public class SweepB2 : MonoBehaviour
 
     private GameObject boss;
     private GameObject player;
-
-    private Vector3[] baseDirections = new Vector3[8];
-
-    private bool isReady;
+    
+    // Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         boss = GameObject.Find("Boss");
         player = GameObject.Find("Player");
-
-
-
-        isReady = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isReady)
-        {
-            Vector3 _movement;
-            _movement = boss.GetComponent<MoveB2>().GiveDirection();
-            AnimationStart(_movement);
-        }
-    }
-    
-    void AnimationStart(Vector3 movement)
-    {
-        
+
     }
 
     public void Sweep() {
         // TODO - 애니메이션 시작
-        isReady = true;
+       
     }
 }
