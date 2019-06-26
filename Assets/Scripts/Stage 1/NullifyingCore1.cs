a99247cf4ab050284a7d11199b8f0c148ea4b857
Laid41798175 <51368746+Laid41798175@users.noreply.github.com>
2019-06-27 01:40:52 +0900

    asdf

---
+++ Assets/Scripts/Stage 1/NullifyingCore1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NullifyingCore1 : MonoBehaviour
+{
+    public Image nullifyingCore;
+
+    public GameObject smoke;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.tag == "Player") {
+            Instantiate(smoke, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+
+            nullifyingCore.enabled = true;
+
+            StartCoroutine(SceneChange());
+        }
+    }
+
+    IEnumerator SceneChange() {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene("Main Menu");
+    }
+}
